7daf6c779eb4dfbcdbcee1ddbc9b1ae51f8c58f1
Evgeniy Gusev <ag.evgeniy@mail.ru>
2015-08-25 12:58:54 +0400

    Added session counter on index page.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = count_increment
+  end
+  def count_increment
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
